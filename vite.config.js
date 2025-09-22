a62e9a66694b4fc610d636c55480d5372ef3645d
Amol Jadhav <amolj9238@gmail.com>
2025-09-23 01:48:55 +0530

    modifying the route1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/freshers_fiesta/' // repo name for GitHub Pages
+  base: 'freshers_fiesta' // repo name for GitHub Pages
 })
