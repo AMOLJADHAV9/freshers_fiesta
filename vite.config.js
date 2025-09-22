4f353795bbd7331d24c95299fb8949547157334f
Amol Jadhav <amolj9238@gmail.com>
2025-09-23 01:36:15 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fresher_fiesta-2k25/' // repo name for GitHub Pages
+  base: '/freshers_fiesta' // repo name for GitHub Pages
 })
