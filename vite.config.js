8a6b41ea8b17e310d37184985919922e1cb8efda
Amol Jadhav <amolj9238@gmail.com>
2025-09-23 01:13:43 +0530

    first commit1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/fresher_fista-2k25/', // <-- Add your repo name here
-  plugins: [react()]
+  plugins: [react()],
+  base: '/fresher_fista-2k25/' // repo name for GitHub Pages
 })
