67b38f3cba422da53e9fb518b133122d72a02c32
Amol Jadhav <amolj9238@gmail.com>
2025-09-23 01:24:03 +0530

    trying to hosting2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fresher_fista-2k25/' // repo name for GitHub Pages
+  base: '/fresher_fiesta-2k25/' // repo name for GitHub Pages
 })
